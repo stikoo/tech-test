eb03038b861849e7a613f57cc793349183dfc38d
Alastair Hodgson <alastair@fortisfar.co.uk>
2018-09-11 13:59:27 +0100

    Style the basket panels and add JS accordion module.

--- Components/Booking/BasketSummary/BasketSummary.php
+++ Components/Booking/BasketSummary/BasketSummary.php
@@ -5,85 +5,86 @@
 
             <h2 class="basket-summary__breakdown-title">Your holiday to [region]</h2>
             <p class="basket-summary__breakdown-details">for 2 Adults, 1 Child, 1 Infant for 10 nights from Wed 14 Jul 2017</p>
-            <a href="#" class="basket-summary__included-btn js-basket-desktop-trigger">
-            <span class="underline">What's included</span> <?php svg('arrow-down-bold', 12, 12, 'basket-summary__included-icon', ''); ?></a>
-
+            <a href="#" class="basket-summary__included-btn basket-summary__included-btn--active js-basket-desktop-trigger">
+                <span class="underline">What's included</span>
+                <?php svg('arrow-down-bold', 12, 12, 'basket-summary__included-icon', ''); ?>
+            </a>
 
             <div class="basket-summary__breakdown-content basket-summary__breakdown-content--visible js-basket-breakdown-content">
                 <p class="basket-summary__breakdown-subtitle">Included in your holiday</p>
 
-                <ul>
-                    <li>
+                <ul class="basket-summary__list">
+                    <li class="basket-summary__list-item">
                         <?php svg('plane2', 12, 12, 'basket-summary__list-icon', ''); ?>
                         Return flights <a href="#" class="js-trigger-modal" data-modal-id="flight-information">Leeds Bradford Airport</a>
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('luggage', 12, 12, 'basket-summary__list-icon', ''); ?>
                         (4) 10kg hand baggage
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('suitcase', 12, 12, 'basket-summary__list-icon', ''); ?>
                         (2) 22kg checked-in bags
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('hotel', 12, 12, 'basket-summary__list-icon', ''); ?>
                         Palladium Hotel Palmrya
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('meal-bold', 12, 12, 'basket-summary__list-icon', ''); ?>
                         All inclusive
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('key', 12, 12, 'basket-summary__list-icon', ''); ?>
                         (1) A room type
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('bus', 12, 12, 'basket-summary__list-icon', ''); ?>
                         Included transfer type
                     </li>
                 </ul>
 
+                <hr class="basket-summary__breakdown-hr">
+
                 <p class="basket-summary__breakdown-subtitle">Extra benefits and special offers</p>
 
-                <ul>
-                    <li>
+                <ul class="basket-summary__list">
+                    <li class="basket-summary__list-item">
                         <?php svg('smile', 12, 12, 'basket-summary__list-icon', ''); ?>
                         (1) Free Child place
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('infant', 12, 12, 'basket-summary__list-icon', ''); ?>
                         (1) Free Infant place
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('ticket', 12, 12, 'basket-summary__list-icon', ''); ?>
                         Excursion Name tickets
                     </li>
-                    <li>
+                    <li class="basket-summary__list-item">
                         <?php svg('resort-flight-checkin2', 12, 12, 'basket-summary__list-icon', ''); ?>
                         Resort Flight Check-In
                     </li>
                 </ul>
+                
+                <hr class="basket-summary__breakdown-hr">
 
+                <p class="basket-summary__breakdown-subtitle">Holiday Essentials</p>
 
-
-                    <p class="basket-summary__breakdown-subtitle">Holiday Essentials</p>
-
-                    <ul>
-                        <li>
-                            <?php svg('tick', 12, 12, 'basket-summary__list-icon', ''); ?>
-                            Hotel extras
-                        </li>
-                        <li>
-                            <?php svg('suitcase', 12, 12, 'basket-summary__list-icon', ''); ?>
-                            (1) Additional checked-in bags
-                        </li>
-                        <li>
-                            <?php svg('golf', 12, 12, 'basket-summary__list-icon', ''); ?>
-                            (1) Golf clubs
-                        </li>
-                    </ul>
-
-
+                <ul class="basket-summary__list">
+                    <li class="basket-summary__list-item">
+                        <?php svg('tick', 12, 12, 'basket-summary__list-icon', ''); ?>
+                        Hotel extras
+                    </li>
+                    <li class="basket-summary__list-item">
+                        <?php svg('suitcase', 12, 12, 'basket-summary__list-icon', ''); ?>
+                        (1) Additional checked-in bags
+                    </li>
+                    <li class="basket-summary__list-item">
+                        <?php svg('golf', 12, 12, 'basket-summary__list-icon', ''); ?>
+                        (1) Golf clubs
+                    </li>
+                </ul>
             </div>
         </div>
 
--- Components/Booking/BasketSummary/js/basketSummary.js
+++ Components/Booking/BasketSummary/js/basketSummary.js
@@ -1,3 +1,24 @@
 import $ from 'jquery';
 
-console.log('nick');
+$(() => {
+    $('.js-basket-desktop-trigger').on('click', function accordionHandler(e) {
+        e.preventDefault();
+
+        $(this)
+            .find('.basket-summary__included-icon')
+            .toggleClass('basket-summary__included-icon--rotated')
+            .end()
+            .next('.js-basket-breakdown-content')
+            .toggleClass('basket-summary__breakdown-content--visible');
+    });
+
+    // In real life I would probably abstract the accordion functionality into a more generic,
+    // reusable piece of code and also apply it to the 'Holidy Essentials' panel, however, the brief was
+    // limited in scope and there's only so much you can do in an 1.5 hours :D
+
+    // You would probably also want to have aria attribute manipulation and at least some :focus
+    // states for better accessiblity.
+
+    // I was also tempted to use slideToggle(), but the way in which the CSS was modelled looked to
+    // favour CSS element state rather than jS driven state changes.
+});
